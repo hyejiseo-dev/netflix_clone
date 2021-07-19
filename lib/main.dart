a60f78c944fd409c6ad31e02585c3301e15e6650
hyejiseo-dev <dianna537@gmail.com>
2021-07-20 01:09:31 +0900

    add TopBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:netflix_clone_coding/screen/home_screen.dart';
 import 'package:netflix_clone_coding/widget/bottom_bar.dart';
 
 void main() {
@@ -16,19 +17,18 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       title: 'NetFlix',
       theme: ThemeData(
+          //앱 테마색 지정
           brightness: Brightness.dark,
           primaryColor: Colors.black,
           accentColor: Colors.white),
       home: DefaultTabController(
-        length: 4,
+        length: 4, //4개의 영역으로 나눈다
         child: Scaffold(
           body: TabBarView(
-            physics: NeverScrollableScrollPhysics(),
+            //tab바를 바디에 넣어준다(4개의 영역마다 다른 화면 표시)
+            physics: NeverScrollableScrollPhysics(), //스크롤이 되지않도록
             children: <Widget>[
-              Container(
-                  child: Center(
-                child: Text('Home'),
-              )),
+              HomeScreen(), //home_screen.dart
               Container(
                   child: Center(
                 child: Text('Search'),
@@ -43,7 +43,7 @@ class _MyAppState extends State<MyApp> {
               ))
             ],
           ),
-          bottomNavigationBar: Bottom(),
+          bottomNavigationBar: Bottom(), //bottom_bar.dart파일에서 쓴 Bottom을 가져와서 넣기
         ),
       ),
     );
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initStatue() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TopBar();
+  }
+}
+
+class TopBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(20, 7, 20, 7),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween, //화면 크기에 맞춰 나열시킴
+        children: <Widget>[
+          Image.asset(
+            'images/bbongflix_logo.png', //pubspec.yaml의 asset에 이미지 등록 후 가져오기
+            fit: BoxFit.contain,
+            height: 25,
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 1),
+            child: Text(
+              'TV 프로그램',
+              style: TextStyle(fontSize: 14),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 1),
+            child: Text(
+              '영화',
+              style: TextStyle(fontSize: 14),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 1),
+            child: Text(
+              '내가 찜한 콘텐츠',
+              style: TextStyle(fontSize: 14),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
