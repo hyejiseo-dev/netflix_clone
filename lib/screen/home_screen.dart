d480bbb2e9c3319ad419a757acdb8444c6ef42b5
hyejiseo-dev <dianna537@gmail.com>
2021-07-21 00:55:42 +0900

    add circle_slider

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:netflix_clone_coding/model/model_movie.dart';
 import 'package:netflix_clone_coding/widget/carousel_slider.dart';
+import 'package:netflix_clone_coding/widget/circle_slider.dart';
 
 class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
@@ -49,7 +50,10 @@ class _HomeScreenState extends State<HomeScreen> {
             CarouselImage(movies: movies),
             TopBar(),
           ],
-        )
+        ),
+        CircleSlider(
+          movies: movies,
+        ),
       ],
     );
   }
