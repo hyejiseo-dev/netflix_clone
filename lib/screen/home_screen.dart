f2fc4efe359250ae8af8da21d25a2a822d22479f
hyejiseo-dev <dianna537@gmail.com>
2021-07-20 22:51:32 +0900

    use carousel_slider and add images

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:netflix_clone_coding/model/model_movie.dart';
+import 'package:netflix_clone_coding/widget/carousel_slider.dart';
 
 class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<Movie> movies = [
+    //모델을 선언해 더미 데이터 만듬 > 파이어베이스 연동을 위함
+    Movie.fromMap({
+      'title': '사랑의 불시착',
+      'keyword': '사랑/로맨스/판타지',
+      'poster': 'test_movie_1.png',
+      'like': false
+    })
+  ];
+
   @override
   void initStatue() {
     super.initState();
@@ -12,7 +24,16 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return TopBar();
+    return ListView(
+      children: <Widget>[
+        Stack(
+          children: <Widget>[
+            CarouselImage(movies: movies),
+            TopBar(),
+          ],
+        )
+      ],
+    );
   }
 }
 
